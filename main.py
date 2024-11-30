98473e36865ff5b85b3b6d483005a1314f2fc01f
Edgar Hernandez <113563814+edgarfer03@users.noreply.github.com>
2024-11-30 14:55:36 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,188 @@
+from flask import Flask, render_template, request, jsonify
+from werkzeug.utils import secure_filename
+import base64
+import json
+from openai import OpenAI
+import re
+from supporting import import_google_image
+
+
+
+app = Flask(__name__)
+
+
+with open('./recipes.json', 'r', encoding='utf-8-sig') as file:
+    data = json.load(file)
+    new_id = int(max([recipe['id'] for recipe in data['recipes']])) + 1
+
+
+def modify_recipe(id, attribute, value):
+    print(f"Modifying recipe {id}...")
+    if attribute not in ['title', 'description', 'ingredients', 'instructions', 'image', 'recommended', 'favorite', 'generated']:
+        return -1
+    print('here')
+    for recipe in data['recipes']:
+        if str(recipe['id']).strip() == id.strip():
+            print(f"Updating recipe {id}...")
+            data['recipes'][data['recipes'].index(recipe)][attribute] = value
+            print(f"Recipe {id} updated.")
+            break
+
+    with open('./recipes.json', 'w', encoding='utf-8') as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
+
+def get_recommended():
+    return [recipe for recipe in data['recipes'] if recipe['recommended']]
+
+def get_favorites():
+    return [recipe for recipe in data['recipes'] if recipe['favorite']]
+
+def get_saved_recipes():
+    return data['recipes']
+
+def add_recipes_to_recipes_json(list_recipes):
+    # get file with highest number in recipes.json
+    '''with open('./recipes.json', 'r') as file:
+        data = json.load(file)
+        new_id = int(max([recipe['id'] for recipe in data['recipes']])) + 1'''
+
+    provisional_result = []
+    global new_id
+    global data
+
+    for recipe in list_recipes:
+        # add the recipe to the json file
+        recipe_json = {
+            "title": recipe['title'],
+            "ingredients": recipe['ingredients'],
+            "instructions": recipe['instructions'],
+            "id": new_id,
+            "favorite": False,
+            "recommended": False,
+            "generated": True,
+            "image": import_google_image(recipe['title'], new_id)
+        }
+        data['recipes'].append(recipe_json)
+        provisional_result.append(recipe_json)
+        new_id += 1
+
+    with open('./recipes.json', 'w', encoding='utf-8') as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
+    
+    return provisional_result
+
+
+@app.route('/', methods=['GET'])
+def index():
+    print(request.method)
+
+    return render_template('index.html', favorites=get_favorites(), recommended_recipes=get_recommended())
+
+
+@app.route('/upload_image', methods=['POST'])
+def upload_image():
+
+    data = request.get_json()
+    if not data or 'base64string' not in data:
+        return jsonify({"error": "Invalid input"}), 400
+
+    # Decode the Base64 string (if needed for further processing)
+    base64_string = data['base64string']
+
+    openai = OpenAI(api_key='sk-svcacct-FFybn1Fzd3412SWSL2G8G1yembXYyQSDmGeVi0P-22iT4qS-VbxKtJ4_AzrfE5T3BlbkFJfDeIaOptGcjfLKZ6GClrZrHv9sIM4JBeYPxiQzm3sHG-rPDT8cfO4LD4zEGg4A')
+    response = openai.chat.completions.create(
+    model="gpt-4o",
+    messages=[
+        {"role": "user", "content": [
+            {"type": "image_url",
+            "image_url": {
+                "url": f"{base64_string}"
+            }}
+        ]}, {"role": "system", "content": 'You will be passed an image containing various ingredients. (it is possible that the image will depict the interior of a fridge). Your job is to identify all the different ingredients in the picture. The response MUST be in JSON, in such a way that the response could be immediately parsed as json without rasing any errors. The form of the JSON structure should be like the following: {"ingredientsRecognized": ["celery", "tomato", "cheese", "cream", "lettuce", "milk", "garlic", "onion", "bread"]}. The response should contain the JSON and nothing else.'}
+    ])
+    
+    ingredients = response.choices[0].message.content
+
+    json_match = re.search(r'{.*}', ingredients, re.DOTALL)
+
+    if json_match:
+        json_string = json_match.group(0) 
+
+    print(ingredients)
+
+    return jsonify({"ingredients": json_string}), 200
+
+
+@app.route('/toggle_favorite', methods=['POST'])
+def toggle_favorite():
+    data = request.json
+    recipe_id = data['recipe_id']
+    is_favorite = data['is_favorite']
+    print(f'is_favorite: {is_favorite}')
+    print('recipe_id: ', recipe_id)
+
+    if is_favorite:
+        # Add to favorites
+        recipe = next((r for r in get_saved_recipes() if str(r['id']).strip() == str(recipe_id).strip()), None)
+        modify_recipe(recipe_id, 'favorite', True)
+        print("recipe")
+        print(recipe)
+
+    else:
+        # Remove from favorites
+        modify_recipe(recipe_id, 'favorite', False)
+    
+    return jsonify({"success": True, "recipe": recipe if is_favorite else None})
+
+@app.route('/get_recipe')
+def get_recipe():
+    recipe_id = request.args.get('recipe_id')
+    
+    recipe = next((r for r in get_saved_recipes() if str(r['id']).strip() == str(recipe_id).strip()), None)
+
+    if not recipe:
+        print({"error": "Recipe not found."})
+    return jsonify(recipe)
+
+@app.route('/get_favorites_json')
+def get_favorites_json():
+    return jsonify(get_favorites())
+
+@app.route('/find_recipes', methods=['POST'])
+def fetch_recipes():
+    ingredients_list = request.get_json()["ingredients"]
+
+    if not ingredients_list:
+        return jsonify({"error": "Invalid input"}), 400
+
+    openai = OpenAI(api_key='sk-svcacct-FFybn1Fzd3412SWSL2G8G1yembXYyQSDmGeVi0P-22iT4qS-VbxKtJ4_AzrfE5T3BlbkFJfDeIaOptGcjfLKZ6GClrZrHv9sIM4JBeYPxiQzm3sHG-rPDT8cfO4LD4zEGg4A')
+    response = openai.chat.completions.create(
+    model="gpt-4o",
+    messages=[
+        {"role": "user", "content": f"{ingredients_list}"}, 
+        {"role": "system", "content": 'You will be passed a list of ingredients, you will need to suggest 1-3 recipes that you could make from the given ingredients. Minor ingredients like condiments can be assumed. The response MUST be in JSON, in such a way that the response could be immediately parsed as json without rasing any errors. The form of the JSON structure should be like the following: { "recipes": [{"title": "Title of the recipe", "ingredients": ["tomato", "cheese", "bread"], "instructions": "Make the..."}, {"title": "Title of the recipe2", "ingredients": ["lettuce", "cream"], "instructions": "Mix the..."}]}. The instructions for each recipe must be clear and about 1-3 paragraphs in length. The response should be the JSON and NOTHING else.'}
+    ])
+    
+    recipes = response.choices[0].message.content
+
+    json_match = re.search(r'{.*}', recipes, re.DOTALL)
+
+    if json_match:
+        json_recipes = json_match.group(0) 
+
+    output_recipes = json.loads(json_recipes)
+    list_recipes = output_recipes['recipes']
+
+    list_recipes = add_recipes_to_recipes_json(list_recipes)
+    
+    print('list_recipes: ')
+    print(list_recipes)
+
+    return jsonify({"recipes": list_recipes}), 200
+
+
+
+
+if __name__ == '__main__':
+
+    app.run(debug=True)
---
+++ supporting.py
@@ -0,0 +1,61 @@
+import json
+import requests
+import os
+
+
+def import_google_image(query, id):
+
+    # Your Custom Search API key and CX (Custom Search Engine ID)
+    API_KEY = 'AIzaSyCpIlmS0DLuekfsDRImLje5_jPN9ZpqbbA'
+    CX = '65ceb5607416f4c51'
+
+    # Request URL
+    url = f"https://www.googleapis.com/customsearch/v1?q={query.replace(' ', '+')}&cx={CX}&searchType=image&key={API_KEY}"
+
+    # Send GET request
+    response = requests.get(url)
+    data = response.json()
+
+    # Download and save the first image
+    if 'items' in data:
+        for item in data['items']:
+            image_url = item['link']
+            print(f"Image found: {image_url}")
+            img_response = requests.get(image_url, headers={
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36'
+            }, allow_redirects=True)
+            print(f"Content-Type: {img_response.headers['Content-Type']}")
+            if 'image' not in img_response.headers['Content-Type']:
+                print("Not an image. Skipping...")
+                continue
+            file_type = image_url.split('.')[-1]
+            if file_type != 'jpg' and file_type != 'jpeg' and file_type != 'png':
+                print("Not a supported image type. Skipping...")
+                continue
+            # Save the image
+            with open(f"./static/images/{id}.{file_type}", 'wb') as f:
+                f.write(img_response.content)
+            print("Image saved successfully!")
+            break
+    else:
+        print("No image found.")
+
+    return f"./static/images/{id}.{file_type}"
+
+
+'''with open('./recipes.json', 'r', encoding='utf-8-sig') as file:
+    data = json.load(file)
+
+
+for recipe in data['recipes']:
+    print(recipe['title'])
+    file_type = import_google_image(recipe['title'], recipe['id'])
+    data['recipes'][data['recipes'].index(recipe)]['image'] = f'./static/images/{recipe["id"]}.{file_type}'
+
+
+with open('./recipes.json', 'w', encoding='utf-8') as file:
+    json.dump(data, file, ensure_ascii=False, indent=4)'''
+
+
+
+
